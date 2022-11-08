c6123e3309d1cd9529ccd52177bf19024c391c35
Nazareno Valentini <nazareno.valentini@gmail.com>
2022-11-08 11:28:01 +0100

    add firestore logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import  { AuthProvider } from "./components/context/AuthContext";
+import { AuthProvider } from "./components/context/AuthContext";
 import Welcome from "./components/pages/Welcome";
 import Main from "./pages/Main"
 import Home from "./pages/Home";
@@ -11,16 +11,17 @@ export default function App() {
 
 
   return (
-
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/main' element={<Main />} />
-        <Route path='/main/AddScene' element={<AddScene />} />
-      </Routes>
-    </AuthProvider>
-  );
+    <>
+      <AuthProvider>
+        <Routes>
+          <Route path="/Welcome" element={<Welcome />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/main/AddScene' element={<AddScene />} />
+        </Routes >
+      </AuthProvider>
+    </>
+  )
 }
 
 
