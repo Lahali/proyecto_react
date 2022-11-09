3919da5160763d6e76c8a171a1faf5eb22c03fca
Nazareno Valentini <nazareno.valentini@gmail.com>
2022-11-09 11:42:30 +0100

    move Map to other component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,10 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
 import { useState } from 'react';
-import DisplayMarkers from '../components/DisplayMarkers';
 import Navbar from '../components/Navbar';
 import Card from '../components/Card';
 import { useLocation } from 'react-router-dom' // esto para importar "props" con Link
-import NewMarker from '../components/NewMarker'
-
+import Map from '../components/Map';
 
 export default function Main() {
-
-  const [newMarkerPosition, setNweMarkerPosition] = useState();
 
   const location = useLocation()
   const { film } = location.state;
@@ -19,14 +14,6 @@ export default function Main() {
   const [currentMarker, setCurrentMarker] = useState({}); // el marker actualmente seleccionado
   //console.log('currentMarker::', currentMarker);
   
-  const mapURL0 = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-  const attrib = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-  const mapURL3 = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  const mapURL1 = 'https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png' // serve token
-  const mapURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-  const mapURL2 = 'https://{s}.tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png' // serve token
-
-
   return (
     <>
       <Navbar
@@ -34,32 +21,13 @@ export default function Main() {
       />
       <div className="mainContainer">
 
-        <div className='mapContainer'>
-          <MapContainer // este componente de React-Leaflet crea el mapa
-            ref={setMap} // whenCreated={setMap} esta era la forma antigua de hacerlo
-            center={[41.4, 2.17]}
-            zoom={12}
-            scrollWheelZoom={true}
-          >
-            <TileLayer // componenete de React-Leaflet para decidir el mapa (url) 
-              attribution={attrib}
-              url={mapURL}
-            />
-
-            <NewMarker // para agregar el mapa para una nueva escena
-              newMarkerPosition={newMarkerPosition}
-              setNweMarkerPosition={setNweMarkerPosition}
-              map={map}
-            />
-
-            <DisplayMarkers // eseña los marker en la mapa
-              title={movie.title}
-              movieFeatures={movie.features}
-              currentMarker={currentMarker}
-              setCurrentMarker={setCurrentMarker}
-            />
-          </MapContainer  >
-        </div>
+        <Map
+        map={map}
+        setMap={setMap}
+        movie={movie}
+        currentMarker={currentMarker}
+        setCurrentMarker={setCurrentMarker}
+        />
 
         <Card // esta es la card que sube desde abajao "estilo Google Maps"
           movie={movie}
